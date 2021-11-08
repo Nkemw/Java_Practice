67a9db16df61f0a2b9233f314500c7d25f5fd989
Nkemw <92009396+Nkemw@users.noreply.github.com>
2021-11-09 01:26:01 +0900

    Add files via upload

---
+++ GUI/JFrame4Demo.java
@@ -0,0 +1,32 @@
+package project;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class JFrame4Demo extends JFrame{
+	public JFrame4Demo() {
+		setTitle("¾È³ç, ½ºÀ®!");
+		
+		JPanel p = new JPanel();
+		JLabel l = new JLabel("¾È³ç, ½ºÀ®!");
+		JButton b = new JButton("¹öÆ°");
+		
+		p.add(l);
+		p.add(b);
+		add(p);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 100);
+		setVisible(true);
+	}
+	
+	
+	public static void main(String[] args) {
+		new JFrame4Demo();
+		
+
+	}
+
+}
