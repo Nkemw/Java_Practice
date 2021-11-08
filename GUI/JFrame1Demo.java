ab131b4669c72856703fd51891edbb9ee68d164e
Nkemw <92009396+Nkemw@users.noreply.github.com>
2021-11-09 01:25:47 +0900

    Create JFrame1Demo.java

---
+++ GUI/JFrame1Demo.java
@@ -0,0 +1,16 @@
+package project;
+
+import javax.swing.JFrame;
+
+class MyFrame extends JFrame{
+	MyFrame(){
+		setTitle("안녕, 스윙!");
+		setSize(300, 100);
+		setVisible(true);
+	}
+}
+public class JFrame1Demo {
+	public static void main(String[] args) {
+		new MyFrame();
+	}
+}
